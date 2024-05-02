1b9830e1c7bd4915de5f518770c9f2f6e57c247d
bgrgicak <berislav.grgicak@gmail.com>
2024-05-02 15:01:15 +0200

    Scan screen start

--- src/views/scan/Scan.tsx
+++ src/views/scan/Scan.tsx
@@ -1,17 +1,107 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
+import { Icon, wordpress } from "@wordpress/icons";
 import { ViewContext } from "../../context/view.ts";
+
+import puzzle from "../../assets/puzzle.svg";
+
+import "./Scan.scss";
 
 export const Scan = () => {
   const { setView } = useContext(ViewContext);
+  const video = useRef(null);
+  const canvas = useRef(null);
+  const overlay = useRef(null);
+
+  useEffect(() => {
+    if (!video.current) {
+      return;
+    }
+    if (!canvas.current) {
+      return;
+    }
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      return;
+    }
+    const videoElement = video.current as HTMLVideoElement;
+    const canvasElement = canvas.current as HTMLCanvasElement;
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {
+          facingMode: "environment",
+        },
+        audio: false,
+      })
+      .then((stream) => {
+        videoElement.srcObject = stream;
+        videoElement.play().catch((err) => {
+          console.error(`An error occurred: ${err}`);
+        });
+      })
+      .catch((err) => {
+        console.error(`An error occurred: ${err}`);
+      });
+    videoElement.addEventListener(
+      "canplay",
+      (ev) => {
+        console.log("canplay");
+        videoElement.setAttribute("width", window.innerWidth.toString());
+        videoElement.setAttribute("height", window.innerHeight.toString());
+        canvasElement.setAttribute("width", window.innerWidth.toString());
+        canvasElement.setAttribute("height", window.innerHeight.toString());
+      },
+      false
+    );
+  }, [canvas, video]);
+
+  // create dark overlay wih hole in the middle
+  useEffect(() => {
+    if (!overlay.current) {
+      return;
+    }
+    const overlayElement = overlay.current as HTMLCanvasElement;
+    const ctx = overlayElement.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+    overlayElement.width = window.innerWidth;
+    overlayElement.height = window.innerHeight;
+    ctx.fillStyle = "rgba(35, 40, 45, 0.9)";
+    ctx.fillRect(0, 0, overlayElement.width, overlayElement.height);
+    ctx.globalCompositeOperation = "xor";
+    // ctx.drawImage(img, 0, 0);
+
+    // ctx.globalCompositeOperation = "source-in";
+    const img = new Image();
+    img.src = puzzle;
+    img.width = window.innerWidth;
+    ctx.drawImage(
+      img,
+      (window.innerWidth - img.width) / 2,
+      (window.innerHeight - img.height) / 2
+    );
+    ctx.fill();
+  }, [overlay]);
 
   const onClick = () => {
     setView("site");
   };
   return (
-    <div className="view view--scan">
-      <button onClick={onClick} className="button button--primary">
-        Take me to my site
+    <article className="view view--scan">
+      <canvas className="scan__overlay" ref={overlay} />
+      <video
+        id="scan__video"
+        ref={video}
+        autoPlay={true}
+        playsInline={true}
+        muted={true}
+      >
+        Video stream not available.
+      </video>
+      <canvas className="scan__canvas" ref={canvas} />
+      <button onClick={onClick} className="button button--primary scan__action">
+        <Icon icon={wordpress} />
+        <span>Take me to my site</span>
       </button>
-    </div>
+    </article>
   );
 };
